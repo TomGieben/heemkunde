3bfee92edf8ddbd59268c7ec849f0cdac5129162
TomGieben <t.gieben@outlook.com>
2024-08-17 17:27:18 +0200

    welcome text added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,12 @@
 @section('title', 'Home')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Welkom, {{ Auth::user()->name }}</h1>
+                <p>Hier kan je alle functionaliteiten van de admin omgeving vinden.</p>
+            </div>
+        </div>
+    </div>
 @endsection

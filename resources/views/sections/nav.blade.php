766e3fbd6e1c72aa83002622d46660bc43b15bb4
TomGieben <t.gieben@outlook.com>
2024-08-17 18:29:43 +0200

    menu text added

--- resources/views/sections/nav.blade.php
+++ resources/views/sections/nav.blade.php
@@ -5,9 +5,10 @@
                 class="d-inline-block align-text-top" style="height: 30px;"> --}}
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        <button class="navbar-toggler d-flex align-items-center justify-content-center" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon text-dark"></span>
+            <span class="navbar-toggler-icon text-dark me-1"></span>
+            Menu
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
